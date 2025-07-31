5896283939af730e2d7357d0831d0e2402ae50f0
JaelSantos01 <20223TN110@utez.edu.mx>
2025-07-31 13:34:06 -0600

    DWI-391 #comment Funciones generadas correctamente

--- frontend/src/modules/auth/components/FormLogin.jsx
+++ frontend/src/modules/auth/components/FormLogin.jsx
@@ -41,17 +41,7 @@ const FormLogin = () => {
               </svg>
             </div>
             <div className={styles.scales}>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="64"
-                height="64"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              >
+              <svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" >
                 <path d="M16 11V3a1 1 0 0 0-1-1h-6a1 1 0 0 0-1 1v8" />
                 <path d="M8 21h8" />
                 <path d="M12 17v4" />
