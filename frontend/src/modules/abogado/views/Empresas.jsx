edddf72486cafdb8ea99b672694074f507a12223
JaelSantos01 <20223TN110@utez.edu.mx>
2025-08-05 11:38:17 -0600

    DWI-378 #comment Se registró la pantalla de contratos para un abogado

---
+++ frontend/src/modules/abogado/views/Empresas.jsx
@@ -0,0 +1,11 @@
+
+const Empresas = () => {
+    return (
+        <div>
+            <h1>Empresas</h1>
+            <p>Visualiza a que empresas les has brindado servicios legales.</p>
+        </div>
+    );
+}
+
+export default Empresas;
