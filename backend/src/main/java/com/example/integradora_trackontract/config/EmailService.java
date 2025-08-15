1e2a41c4aabd1c4157eae7e03f89f6ecb0b8348f
Carlos-GH23 <20223tn124@utez.edu.mx>
2025-08-15 00:43:37 -0600

    DWI-407 Correo de Recuperacion V1.0 Terminado

---
+++ backend/src/main/java/com/example/integradora_trackontract/config/EmailService.java
@@ -0,0 +1,2 @@
+package com.example.integradora_trackontract.config;public class EmailService {
+}
---
+++ backend/src/main/java/com/example/integradora_trackontract/modules/Password_Resets/model/ResetPasswordResponse.java
@@ -0,0 +1,2 @@
+package com.example.integradora_trackontract.modules.Password_Resets.model;public class ResetPasswordResponse {
+}
