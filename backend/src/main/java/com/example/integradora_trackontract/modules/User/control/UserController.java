c2e62e248dd9606598c272debef1ce83e50ac474
Gabriel-Zavala <20213tn045@utez.edu.mx>
2025-08-15 13:31:52 -0600

    errores 2

--- backend/src/main/java/com/example/integradora_trackontract/modules/User/control/UserController.java
+++ backend/src/main/java/com/example/integradora_trackontract/modules/User/control/UserController.java
@@ -37,8 +37,6 @@ public class UserController {
 
     @PostMapping("/save")
     public ResponseEntity<Message> saveUsers(@Validated(UserDTO.Register.class) @RequestBody UserDTO dto) {
-        System.out.println("DEBUG - Llegó al UserController.saveUsers");
-        System.out.println("DEBUG - Usuario a crear: " + dto.getEmail());
         return userService.save(dto);
     }
 
